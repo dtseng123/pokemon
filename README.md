e8812f516d070febfc0f7912694d7824d0069eeb
Darren Tseng <darrentseng@Darrens-MacBook-Pro.local>
2022-05-31 21:06:51 +0100

    readme & type

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # pokemon
+
+The Pokédex has a wealth of information on all the Pokémon creatures from the entire game series. On the main list pages you can see the various stats of each Pokémon. Click or Search a Pokémon's name to see a detailed page with Pokédex data, abilities and moves!
+
+[To get the project up](https://github.com/dtseng123/pokemon/tree/main/pokedex)
--- pokedex/src/components/Pokedex.tsx
+++ pokedex/src/components/Pokedex.tsx
@@ -97,7 +97,7 @@ export const Pokedex = () => {
               The Pokédex has a wealth of information on all the Pokémon
               creatures from the entire game series. On the main list pages you
               can see the various stats of each Pokémon. Click or Search a Pokémon's name
-              to see a detailed page with Pokédex data, ablities and moves!
+              to see a detailed page with Pokédex data, abilities and moves!
             </Typography>
             <Stack
               sx={{ pt: 4 }}
